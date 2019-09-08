0e01add90c08ae4936722f10414314eeba85f919
janainatrilles <55004579+janainatrilles@users.noreply.github.com>
2019-09-08 13:54:45 -0300

    Update Constants.java

--- src/main/java/br/com/inmetrics/cfg/Constants.java
+++ src/main/java/br/com/inmetrics/cfg/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	public static final String CHROMEDRIVER_PATH = Browser.class.getProtectionDomain().getCodeSource().getLocation().getPath() 
 			+ "../../chromedriver/chromedriver.exe";
 	public static final int BROWSER_TIMEOUT_SECONDS = 10;
-	public static final int INMETRICS_HOME_NAVBAR_OFFSET_Y = 92; // em pixels
+	public static final int INMETRICS_HOME_NAVBAR_OFFSET_Y = 200; // em pixels
 	public static int DEFAULT_WAIT_TIMEOUT = 20; // em segundos
 	
 }
